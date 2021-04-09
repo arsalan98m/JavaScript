535e0019e350a0a880da1e9c387824add4db182a
Muhammad Arsalan Manzoor <44143816+arsalan98m@users.noreply.github.com>
2021-04-09 11:46:07 +0500

    Update Object Basics.js

--- Objects/Object Basics.js
+++ Objects/Object Basics.js
@@ -34,25 +34,3 @@ console.log(person['firstName'])
 
 
 
-const john = {
-  firstName: "john",
-  lastName:"anderson",
-  fullName:function(){
-    console.log(this)
-    console.log(`my full name is ${this.firstName} ${this.lastName}`);
-  }
-}
-
-
-
-const bob = {
-  firstName: "bob",
-  lastName:"sander",
-  fullName:function(){
-    console.log(`my full name is bob sanders`);
-  }
-}
-
-
-john.fullName();
-bob.fullName()
